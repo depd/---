863e873db405bc00721a98ab8317409bb66a034b
dwp <894074731@qq.com>
2017-12-15 13:40:33 +0800

    完善新增商户

--- pages/personSea/personSea.js
+++ pages/personSea/personSea.js
@@ -1,66 +1,36 @@
 // pages/personSea/personSea.js
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-  
+  data:{
+    selected1:true,
+    selected2:false,
+    selected3:false,
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
+  selected1: function (e) {
+    var selected1 = this.data.selected1;
+    var selected2 = this.data.selected2;
+    var selected3 = this.data.selected3;
+    this.setData({
+      selected1:true,
+      selected2:false,
+      selected3:false,
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
+  selected2: function (e) {
+    this.setData({
+      selected1:false,
+      selected2:true,
+      selected3:false,
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
+  selected3: function (e) {
+    this.setData({
+      selected1:false,
+      selected2:false,
+      selected3:true,
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  }
 })
--- pages/qrCode/qrCode.js
+++ pages/qrCode/qrCode.js
@@ -3,5 +3,17 @@
 const app = getApp()
 
 Page({
-  
+  data: {
+    selected: 0
+  },
+  toggleTab: function (e) {
+    var index = e.currentTarget.dataset.index;
+    var selected = this.data.selected;
+    if (index == selected) {
+      return;
+    }
+    this.setData({
+      selected: index
+    })
+  },
 })
